c7f7e8ca4241d0911d29b9e52ac999f4a53ec479
motorideosapp <motorideosapp@gmail.com>
2025-10-04 22:15:05 +0300

    Add files via upload

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.moto_ride_os"
+    // ÇÖZÜM: Hata mesajının belirttiği gibi compileSdk'yı 36'ya yükseltiyoruz.
+    compileSdk = 36
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.example.moto_ride_os"
+        minSdk = flutter.minSdkVersion
+        // ÇÖZÜM: targetSdk'yı da compileSdk ile uyumlu hale getiriyoruz.
+        targetSdk = 36
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+        multiDexEnabled = true
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+}
---
+++ android/app/src/main/kotlin/com/example/moto_ride_os/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.moto_ride_os
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
